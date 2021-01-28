7c30a6136dc17b4397f974b344872618de3ea770
Andrea Sansica <sansica.andrea99@gmail.com>
2021-01-28 17:09:58 +0100

    completo

---
+++ laravel/database/seeds/OfficeSeeder.php
@@ -0,0 +1,16 @@
+<?php
+use App\Office;
+use Illuminate\Database\Seeder;
+
+class OfficeSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        factory(Office::class, 10) -> create();
+    }
+}
